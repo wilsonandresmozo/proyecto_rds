eec0b8a373548227c40989a917ee1235294db89e
Wilson Andres Mozo <wilsonandres.mozo@gmail.com>
2026-06-03 22:10:01 -0500

    creacion de tablas cargos empleados y funciones_cargo

---
+++ database/migrations/2026_06_04_022708_create_empleados_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+        $table->id();
+
+        $table->string('nombres');
+        $table->string('apellidos');
+
+        $table->date('fecha_nacimiento');
+        $table->date('fecha_ingreso');
+
+        $table->decimal('salario', 10, 2);
+
+        $table->boolean('estado');
+
+        $table->foreignId('cargo_id')
+              ->constrained('cargos')
+              ->onDelete('cascade');
+
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empleados');
+    }
+};
---
+++ database/migrations/2026_06_04_022804_create_funciones_cargo_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('funciones_cargo', function (Blueprint $table) {
+        $table->id();
+
+        $table->text('descripcion_funcion');
+
+        $table->boolean('estado');
+
+        $table->foreignId('cargo_id')
+              ->constrained('cargos')
+              ->onDelete('cascade');
+
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('funciones_cargo');
+    }
+};
